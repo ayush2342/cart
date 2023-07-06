85791a03fd5f3bccd5da380024445a839b9c627f
ayush2342 <ayushsharma2342@gmail.com>
2023-07-06 13:13:24 +0530

    firebase update

--- src/App.js
+++ src/App.js
@@ -15,16 +15,39 @@ class App extends React.Component {
             products:[],
             loading:true
         }
+        this.db=firebase.firestore()
     }
 
    componentDidMount()
    {
 
-    firebase
-    .firestore()
+    // firebase
+    // .firestore()
+    // .collection('products')
+    // .get()
+    // .then((snapshot) => {
+     
+    //   const products=snapshot.docs.map((doc)=>{
+        
+    //     const data = doc.data();
+    //     data['id']=doc.id;
+    //     return data;
+    //   })
+
+    //   this.setState(
+    //     {
+    //         products,
+    //         loading:false
+    //     }
+    //   )
+    // });
+
+    this.db
     .collection('products')
-    .get()
-    .then((snapshot) => {
+    // .where('price','<',1000)
+    // .where('title','==','Mug')
+    .orderBy('price','desc')
+    .onSnapshot((snapshot) => {
      
       const products=snapshot.docs.map((doc)=>{
         
@@ -48,13 +71,30 @@ class App extends React.Component {
         const {products} =this.state;
 
         const index= products.indexOf(product);
-        products[index].qty+=1;
+        // products[index].qty+=1;
 
-        this.setState(
+        // this.setState(
+        //     {
+        //         products
+        //     }
+        // )
+
+        const docRef = this.db.collection('products').doc(products[index].id)
+
+        docRef
+        .update(
             {
-                products
+                qty:products[index].qty+1
             }
         )
+        .then(()=>
+        {
+            console.log("Updated Successfully")
+        })
+        .catch((error)=>
+        {
+            console.log("Error Occured : ",error)
+        })
 
     }
 
@@ -67,15 +107,32 @@ class App extends React.Component {
         {
             return;
         }
-        products[index].qty-=1;
+        // products[index].qty-=1;
         
 
-        this.setState(
-            {
-                products
+        // this.setState(
+        //     {
+        //         products
 
+        //     }
+        // )
+
+        const docRef = this.db.collection('products').doc(products[index].id)
+
+        docRef
+        .update(
+            {
+                qty:products[index].qty-1
             }
         )
+        .then(()=>
+        {
+            console.log("Updated Successfully")
+        })
+        .catch((error)=>
+        {
+            console.log("Error Occured : ",error)
+        })
 
     }
 
@@ -83,14 +140,26 @@ class App extends React.Component {
     {
         const{products} =this.state;
 
-        const items=products.filter((item)=>{
-            return item.id!==id})
+        // const items=products.filter((item)=>{
+        //     return item.id!==id})
         
 
-        this.setState({
-            products:items
-        })
+        // this.setState({
+        //     products:items
+        // })
 
+        const docRef = this.db.collection('products').doc(id)
+
+        docRef
+        .delete()
+        .then(()=>
+        {
+            console.log("Deleted Successfully")
+        })
+        .catch((error)=>
+        {
+            console.log("Error Occured : ",error)
+        })
 
     }
 
@@ -117,12 +186,36 @@ class App extends React.Component {
 
       return total;
     }
+
+    // addProduct=()=>
+    // {
+    //     this.db
+    //     .collection('products')
+    //     .add(
+    //         {
+    //             title:'Mug',
+    //                 price:'450',
+    //                 qty:10,
+    //                 img:'https://m.media-amazon.com/images/I/61dCM74pPiL._AC_UY1100_.jpg',
+    //         }
+    //     )
+    //     .then((docRef)=>{
+    //         console.log("Product added successfully : ",docRef)
+    //     })
+    //     .catch((error)=>
+    //     {
+    //         console.log("Error Occured : ",error)
+    //     })
+    // }
+
+
     render(){
         const {loading}= this.state;
       return (
         <div className="App" style={{backgroundImage: "url('https://i.pinimg.com/222x/1f/75/1c/1f751cd55f2853a18c2e4a23e01e86a0.jpg')",backgroundSize:'cover'}}>
           <Navbar 
           count={this.getCartCount()}/>
+          {/* <button onClick={this.addProduct}style={{padding:10,fontSize:20}}>Add Product</button> */}
         <Cart
         products={this.state} 
         increaseQuantity={this.handleIncreaseQuantity}
